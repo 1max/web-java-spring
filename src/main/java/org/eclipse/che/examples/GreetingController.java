1a47db2b275d747f527f5bb2a62548d478e13c89
1Max <maximsk@gmail.com>
2016-05-26 22:38:52 +0000

    a

--- src/main/java/org/eclipse/che/examples/GreetingController.java
+++ src/main/java/org/eclipse/che/examples/GreetingController.java
@@ -13,6 +13,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import javax.json.*;
+import java.io.StringReader;
 
 public class GreetingController implements Controller
 {
