bff59ef84865685dc8e788780c246aac80e8e4da
1Max <maximsk@gmail.com>
2016-05-26 18:39:58 +0000

    aa

--- src/main/java/org/eclipse/che/examples/GreetingController.java
+++ src/main/java/org/eclipse/che/examples/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController implements Controller
    @Override
    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
    {
-      System.out.println("TESTTESTTESTTEST" + request.getPathInfo());
+      System.out.println("TESTTESTTESTTEST" + request.toString());
       String userName = request.getParameter("user");
       String result = "";
       if (userName != null)
