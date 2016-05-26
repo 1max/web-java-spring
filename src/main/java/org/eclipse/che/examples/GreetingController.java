e8165b5b7922f0da1ec24c9ac855a42fe4ba1101
1Max <maximsk@gmail.com>
2016-05-26 22:13:58 +0000

    a

--- src/main/java/org/eclipse/che/examples/GreetingController.java
+++ src/main/java/org/eclipse/che/examples/GreetingController.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import javax.json.*;
